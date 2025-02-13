f8374755cb09bab8cc27c7c84a8a2dfcb8046ebc
artur.balsis <artur.balsis@gmail.com>
2025-02-13 11:49:59 +0300

    initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope = "function", autouse = True)
+def init_browser():
+    options = webdriver.ChromeOptions()
+    options.page_load_strategy = 'eager'
+    options.add_argument("--window-size=1920,1080")
+    # options.add_argument('--headless')
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.driver_options = options
+    yield
+    browser.quit()
---
+++ tests/test_practice_form.py
@@ -0,0 +1,7 @@
+import time
+
+from selene import browser
+
+
+def test_fill_registration_form():
+    browser.open("/automation-practice-form")
