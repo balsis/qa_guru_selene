6e09e926ec0777459f14419d4ca35845995a5396
artur.balsis <artur.balsis@gmail.com>
2025-03-05 21:31:15 +0300

    fix video

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,23 +8,23 @@ from helpers import attach
 @pytest.fixture(scope = 'function', autouse = True)
 def remote_browser(request):
     options = webdriver.ChromeOptions()
-    # options.page_load_strategy = 'eager'
-    # options.add_argument("--window-size=1920,1080")
-    # options.add_argument('--headless')
+    browser.config.driver_options = options
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": "125.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
+    options.page_load_strategy = 'eager'
     options.capabilities.update(selenoid_capabilities)
     browser.config.driver = webdriver.Remote(
         command_executor = f"https://{SelenoidData.SELENOID_LOGIN}:{SelenoidData.SELENOID_PASS}@{SelenoidData.SELENOID_URL}/wd/hub",
         options = options)
-    browser.config.driver_options = options
-    browser.config.base_url = 'https://demoqa.com'
     yield browser
     attach.add_screenshot(browser)
     attach.add_html(browser)
