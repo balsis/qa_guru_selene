af4ca4f88857c0d1e2211b7084123a06e1637261
artur.balsis <artur.balsis@gmail.com>
2025-03-05 20:16:45 +0300

    add pytest.ini & modify conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,17 +2,7 @@ import pytest
 from selene import browser
 from selenium import webdriver
 from data.data import SelenoidData
-
-@pytest.fixture(scope = "function", autouse = False)
-def local_browser():
-    options = webdriver.ChromeOptions()
-    options.page_load_strategy = 'eager'
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument('--headless')
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.driver_options = options
-    yield
-    browser.quit()
+from helpers import attach
 
 
 @pytest.fixture(scope = 'function', autouse = True)
@@ -36,4 +26,8 @@ def remote_browser():
         command_executor = f"https://{SelenoidData.SELENOID_LOGIN}:{SelenoidData.SELENOID_PASS}@{SelenoidData.SELENOID_URL}",
         options = options)
     yield
+    attach.add_screenshot()
+    attach.add_html()
+    attach.add_logs()
+    attach.add_video()
     browser.quit()
