7fcfdd466d7a79bf4953758d8b9c10f763cc90c9
artur.balsis <artur.balsis@gmail.com>
2025-03-05 19:48:13 +0300

    add allure steps & selenoid config for jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,37 @@ from selene import browser
 from selenium import webdriver
 
 
-@pytest.fixture(scope = "function", autouse = True)
-def init_browser():
+@pytest.fixture(scope = "function", autouse = False)
+def local_browser():
     options = webdriver.ChromeOptions()
     options.page_load_strategy = 'eager'
     options.add_argument("--window-size=1920,1080")
     options.add_argument('--headless')
     browser.config.base_url = 'https://demoqa.com'
     browser.config.driver_options = options
+    yield
+    browser.quit()
+
+
+@pytest.fixture(scope = 'function', autouse = True)
+def remote_browser():
+    options = webdriver.ChromeOptions()
+    options.page_load_strategy = 'eager'
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument('--headless')
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.driver_options = options
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "125.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    browser.config.driver = webdriver.Remote(
+        command_executor = f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options = options)
     yield
     browser.quit()
--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,7 +1,11 @@
+import allure
+
 from pages.registration_page import RegistrationPage
 
 
-def test_validate_registration_form():
+@allure.feature("Форма регистрации")
+@allure.story("Проверка успешного заполнения формы регистрации")
+def test_registration_form():
     registration_page = RegistrationPage()
     registration_page.open()
     registration_page.fill_firstname("Ivan")
