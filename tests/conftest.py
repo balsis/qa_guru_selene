26ef656cfd1ffdae685fa9c3ba633ce2eb22254c
artur.balsis <artur.balsis@gmail.com>
2025-03-05 21:02:19 +0300

    fix video

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from helpers import attach
 
 
 @pytest.fixture(scope = 'function', autouse = True)
-def remote_browser():
+def remote_browser(request):
     options = webdriver.ChromeOptions()
     options.page_load_strategy = 'eager'
     options.add_argument("--window-size=1920,1080")
@@ -25,7 +25,7 @@ def remote_browser():
         command_executor = f"https://{SelenoidData.SELENOID_LOGIN}:{SelenoidData.SELENOID_PASS}@{SelenoidData.SELENOID_URL}/wd/hub",
         options = options)
     browser.config.driver_options = options
-    yield
+    yield browser
     attach.add_screenshot(browser)
     attach.add_html(browser)
     attach.add_logs(browser)
