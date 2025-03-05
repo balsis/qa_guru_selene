d1fd8011d89c8755ecb455f83688f6a980d276f2
artur.balsis <artur.balsis@gmail.com>
2025-03-05 20:24:42 +0300

    add allure.title and allure.suite

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -5,6 +5,8 @@ from pages.registration_page import RegistrationPage
 
 @allure.feature("Форма регистрации")
 @allure.story("Проверка успешного заполнения формы регистрации")
+@allure.suite("Форма регистрации")
+@allure.title("Заполнение формы регистрации пользователя валидными данными")
 def test_registration_form():
     registration_page = RegistrationPage()
     registration_page.open()
@@ -27,3 +29,5 @@ def test_registration_form():
                                                        'avatar.png',
                                                        'Pushkina street, Kolotushkina house', 'Haryana Panipat')
     registration_page.close_button_should_be_clickable()
+
+
